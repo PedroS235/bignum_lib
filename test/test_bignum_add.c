779c449a15be9ff2b2e23daa426be6652aeed82f
Pedro Soares <pmbs.123@gmail.com>
2024-04-21 15:03:59 +0200

    feat(test): add missing edge cases

--- test/test_bignum_add.c
+++ test/test_bignum_add.c
@@ -3,21 +3,47 @@
 #include "arithmetic.h"
 #include "core.h"
 
-void test_add_basic() {
-    bignum_t a = str2bignum("0");
-    bignum_t b = str2bignum("1");
-    bignum_t c = add(&a, &b);
-    CU_ASSERT(compare_bignum(&b, &c) == 0)
-    free_bignum(&a);
-    free_bignum(&b);
-    free_bignum(&c);
-}
-
 void test_add_simple() {
     bignum_t a = str2bignum("50");
     bignum_t b = str2bignum("40");
     bignum_t c = add(&a, &b);
     bignum_t expected = str2bignum("90");
+    CU_ASSERT(compare_bignum(&c, &expected) == 0)
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&c);
+    free_bignum(&expected);
+}
+
+void test_add_pos_neg() {
+    bignum_t a = str2bignum("18446744073709551615");
+    bignum_t b = str2bignum("-36893488147419103230");
+    bignum_t c = add(&a, &b);
+    bignum_t expected = str2bignum("-18446744073709551615");
+    CU_ASSERT(compare_bignum(&c, &expected) == 0)
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&c);
+    free_bignum(&expected);
+}
+
+void test_add_neg_pos() {
+    bignum_t a = str2bignum("-18446744073709551615");
+    bignum_t b = str2bignum("36893488147419103230");
+    bignum_t c = add(&a, &b);
+    bignum_t expected = str2bignum("18446744073709551615");
+    CU_ASSERT(compare_bignum(&c, &expected) == 0)
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&c);
+    free_bignum(&expected);
+}
+
+void test_add_neg_pos_2() {
+    bignum_t a = str2bignum("-36893488147419103230");
+    bignum_t b = str2bignum("18446744073709551615");
+    bignum_t c = add(&a, &b);
+    bignum_t expected = str2bignum("-18446744073709551615");
     CU_ASSERT(compare_bignum(&c, &expected) == 0)
     free_bignum(&a);
     free_bignum(&b);
@@ -62,8 +88,11 @@ void test_add_w_negative_large() {
 }
 
 void bignum_add_tests_to_suite(CU_pSuite suite) {
-    CU_add_test(suite, "test_bignum_add_basic", test_add_basic);
     CU_add_test(suite, "test_bignum_add_simple", test_add_simple);
     CU_add_test(suite, "test_bignum_add_max_uint64", test_add_max_uint64);
     CU_add_test(suite, "test_bignum_add_w_negative_large", test_add_w_negative_large);
+    CU_add_test(suite, "test_bignum_add_w_negative_simple", test_add_w_negative_simple);
+    CU_add_test(suite, "test_bignum_add_pos_neg", test_add_pos_neg);
+    CU_add_test(suite, "test_bignum_add_neg_pos", test_add_neg_pos);
+    CU_add_test(suite, "test_bignum_add_neg_pos_2", test_add_neg_pos_2);
 }
