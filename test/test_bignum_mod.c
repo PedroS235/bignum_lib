dfa311a32db7635833c21704cb3246adeeef9492
Pedro Soares <pmbs.123@gmail.com>
2024-04-22 18:54:00 +0200

    tests: test division by zero on bignum mod

--- test/test_bignum_mod.c
+++ test/test_bignum_mod.c
@@ -52,15 +52,13 @@ void test_mod_zero_divisor() {
     bignum_t a = str2bignum("10");
     bignum_t b = str2bignum("0");
     bignum_t c;
-    bignum_mod(&c, &a, &b);
-    bignum_t r_c = str2bignum("0");
+    int ret = bignum_mod(&c, &a, &b);
 
-    CU_ASSERT(compare_bignum(&c, &r_c) == 0);
+    CU_ASSERT(ret);
 
     free_bignum(&a);
     free_bignum(&b);
     free_bignum(&c);
-    free_bignum(&r_c);
 }
 
 void test_mod_large() {
@@ -83,7 +81,6 @@ void bignum_mod_tests_to_suite(CU_pSuite suite) {
     CU_add_test(suite, "test_bignum_mod_negative", test_mod_negative);
     CU_add_test(suite, "test_bignum_mod_zero", test_mod_zero);
     // TODO: When the correct behavior is ready, uncomment the test below
-    // CU_add_test(
-    //     suite, "test_bignum_mod_zero_divisor", test_mod_zero_divisor);
+    CU_add_test(suite, "test_bignum_mod_zero_divisor", test_mod_zero_divisor);
     CU_add_test(suite, "test_bignum_mod_large", test_mod_large);
 }
