ae8c64c799f36ff4194bd4d55909eabac0a7e910
Pedro Soares <pmbs.123@gmail.com>
2024-04-22 13:17:19 +0200

    fix: use add unsigned instead of add signed

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
+#include <string.h>
+
 #include "arithmetic.h"
 #include "bignum.h"
+#include "utils.h"
 
 int main() {
     bignum_t a = str2bignum("35");
@@ -9,15 +12,21 @@ int main() {
     bignum_t gcd;
     extended_gcd(&gcd, a, b, &x, &y);
     bignum_t expected_gcd = str2bignum("5");
+    bignum_t expected_x = str2bignum("1");
+    bignum_t expected_y = str2bignum("-2");
 
-    // CU_ASSERT(compare_bignum(&gcd, &expected_gcd) == 0);
+    print_bignum(&gcd);
+    print_bignum(&x);
+    print_bignum(&y);
 
     free_bignum(&a);
     free_bignum(&b);
-    // free_bignum(&x);
-    // free_bignum(&y);
+    free_bignum(&x);
+    free_bignum(&y);
     free_bignum(&gcd);
     free_bignum(&expected_gcd);
+    free_bignum(&expected_x);
+    free_bignum(&expected_y);
 
     return 0;
 }
