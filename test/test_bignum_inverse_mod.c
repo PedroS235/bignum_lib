4c15dd454728348a7606df9c2cde0b91afbdef80
Pedro Soares <pmbs.123@gmail.com>
2024-04-22 15:31:07 +0200

    tests: write more tests to inversemod

--- test/test_bignum_inverse_mod.c
+++ test/test_bignum_inverse_mod.c
@@ -2,6 +2,7 @@
 
 #include "arithmetic.h"
 #include "bignum.h"
+#include "utils.h"
 
 void test_inversemod_simple() {
     bignum_t a = str2bignum("35");
@@ -9,22 +10,100 @@ void test_inversemod_simple() {
     bignum_t mod_inverse;
     inversemod(&mod_inverse, &a, &b);
 
-    // To check the correctness of the inversemod, (mod_inverse * a) % b should be 1
-    bignum_t product;
-    mult_bignum(&product, &mod_inverse, &a);
-    bignum_t remainder;
-    bignum_mod(&remainder, &product, &b);
-    bignum_t one = str2bignum("1");
-    CU_ASSERT(compare_bignum(&remainder, &one) == 0);
+    bignum_t expected = str2bignum("17");
+
+    CU_ASSERT(compare_bignum(&mod_inverse, &expected) == 0);
 
     free_bignum(&a);
     free_bignum(&b);
     free_bignum(&mod_inverse);
-    free_bignum(&product);
-    free_bignum(&remainder);
-    free_bignum(&one);
+    free_bignum(&expected);
+}
+
+void test_inversemod_noinv() {
+    bignum_t a = str2bignum("39");
+    bignum_t b = str2bignum("9");
+    bignum_t mod_inverse;
+    int ret = inversemod(&mod_inverse, &a, &b);
+
+    CU_ASSERT(ret);
+
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&mod_inverse);
+}
+
+void test_inversemod_pos_pos() {
+    bignum_t a = str2bignum("314234123421343243242134123");
+    bignum_t b = str2bignum("3241234");
+    bignum_t mod_inverse;
+    inversemod(&mod_inverse, &a, &b);
+    print_bignum(&mod_inverse);
+    bignum_t expected = str2bignum("1498259");
+
+    CU_ASSERT(compare_bignum(&mod_inverse, &expected) == 0);
+
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&mod_inverse);
+    free_bignum(&expected);
+}
+
+void test_inversemod_pos_neg() {
+    bignum_t a = str2bignum("314234123421343243242134123");
+    bignum_t b = str2bignum("-3241234");
+    bignum_t mod_inverse;
+    inversemod(&mod_inverse, &a, &b);
+    print_bignum(&mod_inverse);
+
+    bignum_t expected = str2bignum("1498259");
+
+    CU_ASSERT(compare_bignum(&mod_inverse, &expected) == 0);
+
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&mod_inverse);
+    free_bignum(&expected);
+}
+
+void test_inversemod_neg_pos() {
+    bignum_t a = str2bignum("-314234123421343243242134123");
+    bignum_t b = str2bignum("3241234");
+    bignum_t mod_inverse;
+    inversemod(&mod_inverse, &a, &b);
+    print_bignum(&mod_inverse);
+
+    bignum_t expected = str2bignum("1498259");
+
+    CU_ASSERT(compare_bignum(&mod_inverse, &expected) == 0);
+
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&mod_inverse);
+    free_bignum(&expected);
+}
+
+void test_inversemod_neg_neg() {
+    bignum_t a = str2bignum("-314234123421343243242134123");
+    bignum_t b = str2bignum("-3241234");
+    bignum_t mod_inverse;
+    inversemod(&mod_inverse, &a, &b);
+    print_bignum(&mod_inverse);
+
+    bignum_t expected = str2bignum("1498259");
+
+    CU_ASSERT(compare_bignum(&mod_inverse, &expected) == 0);
+
+    free_bignum(&a);
+    free_bignum(&b);
+    free_bignum(&mod_inverse);
+    free_bignum(&expected);
 }
 
 void bignum_inversemod_tests_to_suite(CU_pSuite suite) {
-    CU_add_test(suite, "Inverse Mod Simple", test_inversemod_simple);
+    CU_add_test(suite, "test_inversemod_simple", test_inversemod_simple);
+    CU_add_test(suite, "test_inversemod_pos_pos", test_inversemod_pos_pos);
+    CU_add_test(suite, "test_inversemod_pos_neg", test_inversemod_pos_neg);
+    CU_add_test(suite, "test_inversemod_neg_pos", test_inversemod_neg_pos);
+    CU_add_test(suite, "test_inversemod_neg_neg", test_inversemod_neg_neg);
 }
