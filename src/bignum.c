ec5f7b632359582b7290d3b16987354836274028
Pedro Soares <pmbs.123@gmail.com>
2024-04-23 22:14:31 +0200

    chore: rename core to bignum

--- src/bignum.c
+++ src/bignum.c
@@ -1,4 +1,4 @@
-#include "core.h"
+#include "bignum.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,21 +6,7 @@
 
 #include "common.h"
 
-bignum_t init_bignum(int size) {
-    bignum_t num;
-    num.size = size;
-    num.sign = 0;
-    num.digits = (uint8_t *)calloc(size, sizeof(int));
-
-    if (num.digits == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
-
-    return num;
-}
-
-int init_bignum_(bignum_t *num, size_t size, uint8_t sign) {
+int init_bignum(bignum_t *num, size_t size, uint8_t sign) {
     num->size = size;
     num->sign = sign;
     num->digits = (uint8_t *)calloc(size, sizeof(int));
@@ -55,51 +41,7 @@ void resize_bignum(bignum_t *num, size_t new_size) {
     num->size = new_size;
 }
 
-bignum_t str2bignum(char *str) {
-    size_t size = strlen(str);
-    size_t max_digits =
-        size * 4;  // Maximum possible digits based on input string length
-    bignum_t num = init_bignum(max_digits);
-
-    if (str[0] == '-') {
-        num.sign = 1;  // Set sign to negative
-        str++;         // Skip the negative sign character
-        size--;        // Adjust size to exclude the negative sign
-    } else if (str[0] == '+') {
-        num.sign = 0;  // Set sign to positive
-        str++;         // Skip the negative sign character
-        size--;        // Adjust size to exclude the negative sign
-    } else {
-        num.sign = 0;  // Set sign to positive
-    }
-
-    for (size_t i = 0; i < size; i++) {
-        if (str[i] < '0' || str[i] > '9') {
-            printf("Invalid character in string: %c\n", str[i]);
-            exit(1);
-        }
-
-        int digit = str[i] - '0';
-
-        // Multiply existing number by 10 and add the new digit
-        int carry = digit;
-        for (size_t j = 0; j < num.size || carry > 0; j++) {
-            int value = carry;
-            if (j < num.size) value += num.digits[j] * 10;
-            num.digits[j] = value % 2;
-            carry = value / 2;
-        }
-    }
-
-    // Trim leading zeros
-    while (num.size > 1 && num.digits[num.size - 1] == 0) {
-        num.size--;
-    }
-
-    return num;
-}
-
-int str2bignum_(bignum_t *num, char *str) {
+int str2bignum(bignum_t *num, char *str) {
     size_t size = strlen(str);
     size_t max_digits = size * MAX_DIGIT_SIZE;
 
@@ -117,7 +59,7 @@ int str2bignum_(bignum_t *num, char *str) {
         sign = POS;
     }
 
-    init_bignum_(num, max_digits, sign);
+    init_bignum(num, max_digits, sign);
 
     for (size_t i = 0; i < size; i++) {
         if (str[i] < '0' || str[i] > '9') {
