716fb931bfdc75419b9c6fb28bed7b593c5c8926
Pedro Soares <pmbs.123@gmail.com>
2024-05-01 15:24:01 +0200

    feat: clean up main.c

--- src/main.c
+++ src/main.c
@@ -11,17 +11,28 @@ int main() {
     bignum_t e = bignum_new();
     bignum_t d = bignum_new();
 
-    printf("Generating RSA keys...\n");
+    printf("|------------------------------------------------------------------|\n");
+    printf(
+        "| This is an example of our big number library, where we encrypt   |\n| and "
+        "decrypt a message using RSA.                                 |\n");
+
+    printf("|                                                                  |\n");
+
+    printf(
+        "| This project has been developed by Pedro Soares and Issam Jomaa, |\n| "
+        "part "
+        "of Algorithms for Numbers & Public-Key Cryptography course. |\n");
+    printf("|------------------------------------------------------------------|\n");
+
+    printf("\nGenerating RSA keys...\n");
 
     rsa_keygen(&n, &e, &d, 20);
 
     printf("n: ");
     print_bignum(&n);
-    printf("\n");
 
     printf("e: ");
     print_bignum(&e);
-    printf("\n");
 
     printf("d: ");
     print_bignum(&d);
@@ -30,17 +41,21 @@ int main() {
     bignum_t m = bignum_new();
     str2bignum(&m, "1234567890");
 
+    printf("Encrypting message -> ");
+    print_bignum(&m);
     bignum_t c = bignum_new();
     rsa_encrypt(&c, &m, &n, &e);
 
-    printf("Encrypted message: ");
+    printf("Encrypted message -> ");
     print_bignum(&c);
     printf("\n");
 
+    printf("Decrypting -> ");
+    print_bignum(&c);
     bignum_t decrypted = bignum_new();
     rsa_decrypt(&decrypted, &c, &n, &d);
 
-    printf("Decrypted message: ");
+    printf("Decrypted message -> ");
     print_bignum(&decrypted);
     printf("\n");
 
